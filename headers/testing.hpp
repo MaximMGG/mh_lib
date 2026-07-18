0a1227263e0e4b36629a4a26de392b440a0fc892
MaximMGG <maxhrunenko@gmail.com>
2026-07-18 19:21:05 +0300

    testing module in process

--- headers/testing.hpp
+++ headers/testing.hpp
@@ -1,19 +1,30 @@
 #ifndef MH_TESTING_HPP
 #define MH_TESTING_HPP
+#include "core/types.hpp"
 
 
+struct TestCase {
+  i8 *test_name;
+  void (*test_func)();
+  u64 start_time;
+  u64 end_time;
+  bool success;
+};
 
-#endif //MH_TESTING_HPP
+TestCase *prepareTestCases(i8 *tests, i32 test_list_len, ...);
+
 
 #define TEST(test_name) void test_name()
-
 #define TEST_LIST(tests)                                                       \
   int main() {                                                                 \
     int test_list_len = 0;                                                     \
-    char **list = prepareTestCases(#tests, &test_list_len);                    \
-    runTest(list, test_list_len);                                              \
+    TestCase **list = prepareTestCases(#tests, &test_list_len, tests, nullptr);	\
+    runTest(list, test_list_len, __FILE__);					\
     cleanup(list, test_list_len);                                              \
 }
 
 
 #define assertNotNull(p) __assertNotNull(p, __LINE__)
+
+
+#endif //MH_TESTING_HPP
--- src/testing.cpp
+++ src/testing.cpp
@@ -1,5 +1,15 @@
 #include "../headers/testing.hpp"
 #include "../headers/core/types.hpp"
+#include <stdarg.h>
+#include <time.h>
+#include <stdio.h>
+
+#define 
+
+u32 passed_test = 0;
+u32 succesed_test = 0;
+u32 failed_test = 0;
+TestCase *cur_test_case;
 
 u32 testsLen(i8 *tests) {
   i8 *tmp = tests;
@@ -15,31 +25,63 @@ void __strcpy(i8 *dest, i8 *src, u32 len) {
   dest[len] = '\0';
 }
 
-char **prepareTestCases(i8 *tests, int *test_list_len) {
+void runTest(TestCase *test_cases, i32 test_list_len, i8 *test_file_name) {
+  printf("Run %s tests", test_file_name);
+  u64 start_time = clock();
+  for (i32 i = 0; i < test_list_len; i++) {
+    cur_test_case = &test_cases[i];
+    cur_test_case->start_time = clock();
+    cur_test_case->test_func();
+    cur_test_case->end_time = clock();
+    passed_test++;
+    if (cur_test_case->success) {
+      succesed_test++;
+    }
+  }
+  u64 end_time = clock();
+}
+
+TestCase *prepareTestCases(i8 *tests, int *test_list_len, ...) {
   u32 tests_len = testsLen(tests);
   for(i32 i = 0; i < tests_len; i++) {
     if (tests[i] == ',') {
       *test_list_len += 1;
     }
   }
-  char **test_list = new char * [*test_list_len];
+  TestCase *test_list = new TestCase [*test_list_len];
   u32 test_i = 0;
 
   i8 buf[128]{0};
   i32 buf_i = 0;
 
-
   for(i32 i = 0; i < tests_len; i++) {
     if (tests[i] == ',') {
-      test_list[test_i] = new char [buf_i + 1];
-      __strcpy(test_list[test_i], buf, buf_i);
+      test_list[test_i].test_name = new char [buf_i + 1];
+      __strcpy(test_list[test_i].test_name, buf, buf_i);
       ZERO(buf, 128);
       buf_i = 0;
+      test_i++;
     } else {
       buf[buf_i++] = tests[i];
     }
   }
-  
+  test_list[test_i].test_name = new char[buf_i + 1];
+  __strcpy(test_list[test_i].test_name, buf, buf_i);
 
-  return nullptr;
+  test_i = 0;
+  va_list li;
+  va_start(li, test_list_len);
+
+  ptr p;
+
+  while (true) {
+    p = va_arg(li, ptr);
+    if (p == nullptr) {
+      break;
+    }
+    test_list[test_i++].test_func = (void (*)())p;
+  }
+  test_list[0].success = true;
+  va_end(li);
+  return test_list;
 }
